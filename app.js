08a8d319da14ced0c6b6e69a194c06c98fc18510
Daniel Mark Schwartz <mrdschwartz@gmail.com>
2015-08-10 14:45:48 +0900

    Pull user profile data from database

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 var express = require('express');
 var app = express();
+var db = require('mongoskin').db('mongodb://localhost:27017/pairgramming');
+
+
+db.collection('mamals').find().toArray(function(err, result) {
+    if (err) throw err;
+    console.log(result);
+});
 
 app.set('view engine', 'jade');
 app.set('views', './views')
 app.get('/', function (req, res) {
-    res.render('profile', { title: 'Hey', message: 'Hello there!'});
+    db.collection('users').find().toArray(function(err, docs){
+        if(err) throw err;
+        res.render('profile', docs[0]);
+    }); 
 });
 app.use(express.static('static'))
 
