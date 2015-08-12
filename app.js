2233a2fcdd38ca8dc0258abaaf2e7dd85fe42d12
Daniel Mark Schwartz <mrdschwartz@gmail.com>
2015-08-12 11:31:59 +0900

    Correlate /user/:id with each user's profile

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 var express = require('express');
 var app = express();
-var db = require('mongoskin').db('mongodb://localhost:27017/pairgramming');
-
-
-db.collection('mamals').find().toArray(function(err, result) {
-    if (err) throw err;
-    console.log(result);
-});
+var ms = require('mongoskin')
+var ObjectId = require('mongodb').ObjectID
+var db = ms.db('mongodb://localhost:27017/pairgramming');
 
 app.set('view engine', 'jade');
 app.set('views', './views')
-app.get('/', function (req, res) {
-    db.collection('users').find().toArray(function(err, docs){
+
+app.get('/user/:id', function (req, res) {
+    db.collection('users').find({'_id':ObjectId(req.params.id)}).toArray(function(err, docs){
         if(err) throw err;
-        res.render('profile', docs[0]);
+
+        if(docs.length <= 0) {
+            res.sendStatus(404);
+        }else {
+            res.render('profile', docs[0]);
+        }
     }); 
 });
 app.use(express.static('static'))
