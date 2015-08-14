3dbe24757765dcad82c385d217b1c532ffefaa5e
Ronald Waite <waiteforme15@gmail.com>
2015-08-14 23:13:26 +0900

    Rewrite schedule.js from scratch
    After this rewrite, the schedule.js script works as expected for both
    box selection and wrap-around selection of available time.

--- static/schedule.js
+++ static/schedule.js
@@ -1,92 +1,61 @@
 $(document).ready(function() {
-    var isHeld = false; //True if the mouse button is held down still
-    var toggleStatus = true;
-    var clickedTD = 0;
-    var clickedTR = 0;
-    var enteredTD = 0;
-    var enteredTR = 0;
-    var betweenTD = 0;
-    var startTD = 0;
-    var lastTDR = 0;
-    var lastTDL = 100;
-    $('td').mousedown(function() {
-        clickedTD = 0;
-        clickedTR = 0;
-        enteredTD = 0;
-        enteredTR = 0;
-        betweenTD = 0;
-        startTD = 0;
-        lastTDR = 0;
-        lastTDL = 100;
-
-        clickedTD = $(this).prevAll().length;
-        clickedTR = $(this).parent().prevAll().length;
-        lastTDR = clickedTD;
-        lastTDL = clickedTD;
-        if (clickedTD > 0 && clickedTR > 0) {
-            isHeld = true;
-            toggleStatus = $(this).hasClass('vavail');
-            if (toggleStatus) {
-                $(this).removeClass('vavail');
-            } else {
-                $(this).addClass('vavail');
-            }
-        }
-    });
-    $('td').mouseup(function() {
-        isHeld = false;
-        clickedTD = 0;
-        clickedTR = 0;
-        enteredTD = 0;
-        enteredTR = 0;
-        betweenTD = 0;
-        startTD = 0;
-        lastTDR = 0;
-        lastTDL = 100;
-    });
-    $('td').mouseenter(function() {
-        if (isHeld) {
-            enteredTD = $(this).prevAll().length;
-            enteredTR = $(this).parent().prevAll().length;
-            if (lastTDR > enteredTD && enteredTD >= clickedTD && enteredTR == clickedTR) {
-                for (var i = enteredTD; i <= lastTDR; i++) {
-                    if (clickedTD != i) {
-                        $(this).parent().children().eq(i).removeClass('vavail');
-                    }
-                } 
-            } 
-            if (lastTDL < enteredTD && enteredTD <= clickedTD && enteredTR == clickedTR) {
-                for (var j = lastTDL; j <= enteredTD; j++) {
-                    if (clickedTD != j) {
-                        $(this).parent().children().eq(j).removeClass('vavail');
-                    }
-                }
-            }
-            if (enteredTR == clickedTR) {
-                if (enteredTD > clickedTD) {
-                    lastTDR = enteredTD;
-                } else if (enteredTD < clickedTD) {
-                    lastTDL = enteredTD;
-                }
-            }
-            if (enteredTD != 0 && enteredTR == clickedTR) {
-                if (enteredTD > clickedTD) {
-                    betweenTD = enteredTD - clickedTD;
-                    startTD = clickedTD;
-                } else {
-                    betweenTD = clickedTD - enteredTD;
-                    startTD = enteredTD;
-                }
-                if (betweenTD > 0) {
-                    for (var i = startTD; i <= (startTD + betweenTD); i++) {
-                        if (toggleStatus) {
-                            $(this).parent().children().eq(i).removeClass('vavail');
-                        } else {
-                            $(this).parent().children().eq(i).addClass('vavail');
-                        }
-                    }
-                }
-            }
-        }
-    });
+   $('table.hours').each(function(_, table) {
+       var xInit, xFinal, yInit, yFinal, toggleStatus;
+       function graphHours() {
+           // for (rows)
+           //   for (columns) start at initial end at final
+           //       if final > initial block selection
+           //       
+           $('td', table).removeClass('selected');
+           var yStart = yInit >= yFinal ? yFinal + 1 : yInit + 1; //sets the starting row to the lowest value
+           var yFinish = yInit >= yFinal ? yInit + 1 : yFinal + 1;
+           yFinish = (xInit > xFinal)?yFinish-1:yFinish;
+           for (var i = yStart; i <= yFinish; i++) {
+                var j = xInit;
+                var k = 0;
+                do {
+                   $('tbody', table).children().eq(i+k).children().eq(j+1).addClass('selected');
+                   j++;
+                   if(j > 24){k++; j=0;}
+                } while (j != xFinal + 1);//((xFinal >= xInit && j <= xFinal) || (xFinal < xInit && ((j >= xInit && j < 24) || (j <= xFinal))));
+           }
+       }
+       $('td.cell', this).mousedown(function() {
+           xInit = $(this).prevAll().length - 1;
+           xFinal = xInit;
+           yInit = $(this).parent().prevAll().length - 1;
+           yFinal = yInit;
+           toggleStatus = $(this).hasClass('vavail');
+           graphHours();
+           //console.log('mousedown');
+           $('td', table).on('mouseup.hours', function() {
+               //console.log('mouseup');
+               $selected = $('.selected', table);
+               $selected.removeClass('selected');
+               if (!toggleStatus) {
+                   $selected.addClass('vavail');
+               } else {
+                   $selected.removeClass('vavail');
+               }
+               $(table).off('.hours');
+           });
+           $(table).on('mouseenter.hours', 'td.cell', function() {
+               //console.log('mouseenter');
+               xFinal = $(this).prevAll().length - 1;
+               yFinal = $(this).parent().prevAll().length - 1;
+               graphHours();
+           });
+           $(table).on('mouseleave.hours', function() {
+               //console.log('mouseleave table');
+               $(document).on('mouseup.hours2', function() {
+                   //console.log('mouseup document');
+                   $(table).off('.hours');
+                   $('td', table).removeClass('selected');
+               });
+               $(table).on('mouseenter.hours', function() {
+                   $(document).off('.hours2');
+               });
+           });
+       });
+   });
 });
